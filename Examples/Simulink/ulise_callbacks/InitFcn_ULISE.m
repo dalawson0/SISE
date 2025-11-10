307cc9ea59f5c86209aeaea31643499995e05444
Daniel Lawson <daniellawson@Daniels-MacBook-Pro.local>
2025-11-09 22:59:26 -0500

    Update simulink documentation and .slx of ulise_internal_v0 and updated the ULiSE algorithm doc

--- Examples/Simulink/ulise_callbacks/InitFcn_ULISE.m
+++ Examples/Simulink/ulise_callbacks/InitFcn_ULISE.m
@@ -8,6 +8,14 @@
 % Author: Daniel Lawson
 % Date:   October 10, 2025
 
+%% Simulation Setup Parameter
+
+SimSetup = struct();
+
+SimSetup.Ts = 1e0; % Sampling time
+SimSetup.K= 1e3; % Total number of time steps for simulation
+SimSetup.T = SimSetup.Ts * SimSetup.K; % total simulation time
+SimSetup.t  = (0:SimSetup.K-1)' * SimSetup.Ts; % Time vector
 %% Define Plant Parameter
 
 
@@ -17,26 +25,75 @@ A = [ 0.5    2   0   0   0;
     0      0   0   0.7 1;
     0      0   0   0   0.1 ]; 
 
-B = zeros(size(A,1),1);
+% B = zeros(size(A,1),1);
+% 
+% C = blkdiag(1,1,1,1,1);
+% 
+% D = zeros(size(C,1),1);
+% 
+% G = [ 1  0   -0.3;
+%     1   0   0;
+%     0   0   0;
+%     0   0   0;
+%     0   0   0] ;
+% p = size(G, 2);
+% 
+% H = [ 0  0   1;
+%     0  0   0; 
+%     0  1   0; 
+%     0  0   0; 
+%     0  0   0 ]; 
 
-C = blkdiag(1,1,1,1,1);
+% x0 = zeros(size(A,1),1);
+% 
+% R = 1e-2 * [  1    0   0   0.5 0;
+%             0    1   0   0   0.3; 
+%             0    0   1   0   0; 
+%             0.5  0   0   1   0; 
+%             0    0.3 0   0   1  ];
+% 
+% Q = 1e-4 * [  1    0   0   0   0; 
+%             0    1   0.5 0   0; 
+%             0    0.5 1   0   0; 
+%             0    0   0   1   0; 
+%             0    0   0   0   1  ]; 
+% 
+% 
+xhat0 = zeros(size(A,1),1);
+P_x0 = zeros(size(A));
 
-D = zeros(size(C,1),1);
+% % Transmission zeros
+% t_zeros = tzero(A,G,C,H);
+% if rank([A G; C H]) ~= size(A,1)+size(G,2)
+%     error('Error. System is not strongly detectable.')
+% end
+% 
+% for i = 1:length(t_zeros)
+%     if abs(t_zeros(i)) > 1
+%         error('Error. System is not strongly detectable')
+% 
+%     end
+% end
 
-G = [ 1  0   -0.3;
-    1   0   0;
-    0   0   0;
-    0   0   0;
-    0   0   0] ;
-p = size(G, 2);
+% Number of unkown inputs
+p = 3;
 
-H = [ 0  0   1;
-    0  0   0; 
-    0  1   0; 
-    0  0   0; 
-    0  0   0 ]; 
+% Numbe of known inputs
+m = 0;
 
-x0 = zeros(size(A,1),1);
+% Number of states
+n = 5;
+
+% Number of Outputs
+l = n;
+
+%% Generate the process noise and measurement noise vectors
+% 
+Q = 1e-4 * [  1    0   0   0   0; 
+            0    1   0.5 0   0; 
+            0    0.5 1   0   0; 
+            0    0   0   1   0; 
+            0    0   0   0   1  ]; 
 
 R = 1e-2 * [  1    0   0   0.5 0;
             0    1   0   0   0.3; 
@@ -44,70 +101,26 @@ R = 1e-2 * [  1    0   0   0.5 0;
             0.5  0   0   1   0; 
             0    0.3 0   0   1  ];
 
-Q = 1e-4 * [  1    0   0   0   0; 
-            0    1   0.5 0   0; 
-            0    0.5 1   0   0; 
-            0    0   0   1   0; 
-            0    0   0   0   1  ]; 
+SigmaQ = chol(Q);
+w = (randn(SimSetup.K,n)*SigmaQ)';
 
-% Singular Value Decomposition of H
-r = rank(H);
-[U, S, V] = svd(H);
-
-U1 = U(:, 1:r); 
-U2 = U(:, r+1:end);
-
-Sigma = S(1:r, 1:r);
- 
-V1 = V(:, 1:r);
-V2 = V(:, r+1:end);
-
-xhat0 = zeros(size(A,1),1);
-
-% T1 = U1' - U1'*R*U2*inv(U2'*R*U2)*U2';
-% T2 = U2';
-
-% Transmission zeros
-t_zeros = tzero(A,G,C,H);
-if rank([A G; C H]) ~= size(A,1)+size(G,2)
-    error('Error. System is not strongly detectable.')
-end
-
-for i = 1:length(t_zeros)
-    if abs(t_zeros(i)) > 1
-        error('Error. System is not strongly detectable')
-        
-    end
-end
-
-
-%% Simulation Setup Parameter
-
-SimSetup = struct();
-
-SimSetup.Ts = 1e0; % Sampling time
-SimSetup.K= 1e3; % Total number of time steps for simulation
-SimSetup.T = SimSetup.Ts * SimSetup.K; % total simulation time
-SimSetup.t  = (0:SimSetup.K-1)' * SimSetup.Ts; % Time vector
+SigmaR = chol(R);
+v = (randn(SimSetup.K,l)*SigmaR)';
 
 %% Simulate 'true' known input, u, and unknown input, d. 
-u = zeros(1,SimSetup.K);
 
-d = zeros(size(G,2),SimSetup.K); 
+u = zeros(m,SimSetup.K);
+
+d = zeros(p,SimSetup.K); 
 d(1,500:700) = 1*ones(size(d(1,500:700)));
 
 for j = 100:800
     d(2,j) = 1/700*(j-100);
 end
+
+% Modify the third disturbance component
 d(3,[500:549 600:649 700:749]) = 3*ones(size(d(3,[500:549 600:649 700:749])));
 d(3,[550:599 650:699 750:799]) = -3*ones(size(d(3,[550:599 650:699 750:799])));
-%% Generate the process noise and measurement noise vectors
-
-SigmaQ = chol(Q);
-w = (randn(SimSetup.K,size(A,1))*SigmaQ)';
-
-SigmaR = chol(R);
-v = (randn(SimSetup.K,size(C,1))*SigmaR)';
 
 %% Make timeseries input data 
 
