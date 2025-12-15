dca062a30b1267764e9356978cdcc04061b72cdb
Daniel Lawson <daniellawson@Daniels-MacBook-Pro.local>
2025-12-15 17:16:29 -0500

    Add Content.m and include it in MATLAB project file

---
+++ Contents.m
@@ -0,0 +1,14 @@
+% Simultaneous Input and State Estimation for Linear Dynamical Systems
+% Version 1.0.1 12-Dec-2025 
+%
+% Functions
+%   ULISE.m                                   - Discrete-time zero-step state and one-step input delay estimator.
+%
+% Simulink Models
+%   ULISE_faultIdentification_model.slx       - Fault identification model for ULISE filter.
+%
+% Example Live Scripts
+%   example_ULISE_faultIdentification_MATLAB.mlx
+%                                             - Fault identification using ULISE (MATLAB).
+%   example_ULISE_faultIdentification_Simulink.mlx
+%                                             - Fault identification using ULISE (Simulink).
